9521bb9fc338be84f6a53a9a6e0b2ad85f778f87
JulianAlexander <julian.pineyro@gmail.com>
2019-08-16 15:45:46 +0300

    Created Visualizer component

--- ferrybox/src/components/App.js
+++ ferrybox/src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.styles.scss';
+import Visualizer from './visualizer/visualizer.component';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       NIVA
+      <Visualizer />
     </div>
   );
 }
---
+++ ferrybox/src/components/visualizer/visualizer.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './visualizer.styles.scss';
+
+class Visualizer extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <span>Hello, World!</span>
+      </div>
+    );
+  }
+}
+
+export default Visualizer;
