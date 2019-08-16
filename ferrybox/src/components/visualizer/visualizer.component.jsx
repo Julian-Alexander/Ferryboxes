783663ad0e30aec7f4dff24ec4ccca8255502160
JulianAlexander <julian.pineyro@gmail.com>
2019-08-16 16:08:40 +0300

    Fixed typo at line 6 of Visualizer component 'CanvasJSReact' to 'CanvasJSChart'. Implemented Salinity chart with CanvasJS features Zoom and Pan, crosshair and Tooltip

--- ferrybox/src/components/visualizer/visualizer.component.jsx
+++ ferrybox/src/components/visualizer/visualizer.component.jsx
@@ -1,17 +1,67 @@
 import React from 'react';
 import './visualizer.styles.scss';
-import CanvasJSReact from "../../assets/canvasjs.react";
-import signals from "../../data/signals.json";
+import CanvasJSReact from '../../assets/canvasjs.react';
+import signals from '../../data/signals.json';
 
-const CanvasJSReact = CanvasJSReact.CanvasJSChart;
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+let salinity = [];
 
 class Visualizer extends React.Component {
-  state = {};
+  state = {
+    reload: false
+  };
+
+  componentDidMount() {
+    salinity = signals.t.map(({ measurements, properties }) => {
+      const sal = measurements['FA/ferrybox/CTD/SALINITY'];
+      const date = new Date(properties.datetime);
+      console.log(salinity);
+      return { x: date, y: sal };
+    });
+    this.setState({ reload: true });
+  }
 
   render() {
+    const options = {
+      theme: 'dark2',
+      animationEnabled: true,
+      zoomEnabled: true,
+      title: {
+        text: 'FerryBox'
+      },
+      subtitles: [
+        {
+          text: 'Click and Drag Visualizer to Zoom'
+        }
+      ],
+      axisY: {
+        title: 'Measurements',
+        crosshair: {
+          enabled: true,
+          snapToDataPoint: true
+        }
+      },
+      axisX: {
+        title: 'Time',
+        crosshair: {
+          enabled: true,
+          snapToDataPoint: true
+        }
+      },
+      data: [
+        {
+          yValueFormatString: '#,###,###.##',
+          xValueFormatString: 'DDDD MMM YYYY HH:mm:ss',
+          name: 'Salinity',
+          type: 'line',
+          dataPoints: salinity
+        }
+      ]
+    };
     return (
       <div>
-        <span>Hello, World!</span>
+        <CanvasJSChart options={options} onRef={ref => (this.chart = ref)} />
       </div>
     );
   }
